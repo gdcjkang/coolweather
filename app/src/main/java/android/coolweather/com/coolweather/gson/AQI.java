749f6c36f135add845f690106764717c66854810
xiekang <4888645@qq.com>
2018-02-21 19:26:39 +0800

    增加了天气页面但未调试通

---
+++ app/src/main/java/android/coolweather/com/coolweather/gson/AQI.java
@@ -0,0 +1,13 @@
+package android.coolweather.com.coolweather.gson;
+
+/**
+ * Created by Administrator on 2018/2/20.
+ */
+
+public class AQI {
+    public  AQICity city;
+    public class AQICity{
+        public String aqi;
+        public String pm25;
+    }
+}
